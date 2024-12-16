063a3232d792a0e40e22b3fd1fcd7569bb4b1f2b
Yaparla Bhargavi <yaparlabhargavi023@gmail.com>
2024-12-16 21:11:57 +0530

    Add files via upload

---
+++ binaryoperator.py
@@ -0,0 +1,14 @@
+a = 60 
+b = 13 
+c = a & b 
+print ("a & b : ", c)
+c = a | b 
+print ("a | b : ", c)
+c = a ^ b 
+print ("a ^ b : ", c)
+c = ~a; 
+print ("~a : ", c)
+c = a << 2; 
+print ("a << 2 : ", c)
+c = a >> 2; 
+print ("a >> 2 : ", c)
---
+++ logicaloperator.py
@@ -0,0 +1,4 @@
+x = 5
+y = 10
+if x > 3 or y > 15:
+  print("Both x and y are within the specified range")
